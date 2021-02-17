bb1be217dd5bfa821f244f71bd05a9977360f61a
Esmeralda Nava <ifglitterthen@gmail.com>
2021-02-17 00:16:20 -0600

    FIX: View add activity

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,11 @@
 
-let jqueryvalidation = require('jquery-validation');
+// let jqueryvalidation = require('jquery-validation');
 
-    $("#form_registro").validate();
+//     $("#form_registro  ").validate();
+
+console.log("aquí estoy!");
+
+
+$('.check input:checkbox').click(function() {
+    $('.check input:checkbox').not(this).prop('checked', false);
+}); 
