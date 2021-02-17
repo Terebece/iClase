eb0167fb6b67f1fe3e804c44eedf56c649597d74
Teresa Becerril <terebece1508@ciencias.unam.mx>
2021-02-16 22:01:29 -0600

    agregando landing informativa

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,59 @@
 @extends('layouts.default')
 
 @section('content')
-
-<p class="text-center">LANDING INFORMATIVA SOBRE iClass</p>
+<style>.footer{ position: relative}</style>
+<div class="container contenedor-landig">
+    <br>
+    <div class="contenedor_f4">
+      <h1>¡Organiza tus horarios y tareas con iClase!</h1>  
+    </div>
+    <br><br>
+    <div class="row">
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+        <h2>Uno</h2>
+        <p>Promocionar la aplicación en este espacio.</p>
+      </div>
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+        <h2>Dos</h2>
+        <p>Promocionar la aplicación en este espacio.</p>
+      </div>
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+        <h2>Tres</h2>
+        <p>Promocionar la aplicación en este espacio.</p>
+      </div>
+    </div>
+    <hr class="divisor">
+    <div class="row">
+      <div class="col-md-7">
+        <h2 class="encabezado-landig">Ventaja número uno.</h2>
+        <p>Explicación de la ventaja. Decir porque es buena en tan pocas palabras .............................................................................................. </p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect>
+      </div>
+    </div>
+    <hr class="divisor">
+    <div class="row">
+      <div class="col-md-7 order-md-2">
+        <h2 class="encabezado-landig">Ventaja número dos.</h2>
+        <p>Explicación de la ventaja. Decir porque es buena en tan pocas palabras ...............................................................................................</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect>
+      </div>
+    </div>
+    <hr class="divisor">
+    <div class="row">
+      <div class="col-md-7">
+        <h2 class="encabezado-landig">Ventaja número tres :c </h2>
+        <p>Explicación de la ventaja. Decir porque es buena en tan pocas palabras ..............................................................................................</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect>
+      </div>
+    </div>
+    <div class="divisor-final"></div>
 @stop
