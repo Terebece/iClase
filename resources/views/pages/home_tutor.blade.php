fdf963c50c5a7da7d52de78284ea945fad6c8b0a
Teresa Becerril <terebece1508@ciencias.unam.mx>
2021-02-17 11:10:45 -0600

    Arreglando conflictos

--- resources/views/pages/home_tutor.blade.php
+++ resources/views/pages/home_tutor.blade.php
@@ -1,20 +1,15 @@
-lines (13 sloc)  420 Bytes
+@extends('layouts.default_tutor')
+@section('content')
   
-  @extends('layouts.default_tutor')
-  
-  @section('content')
-  
-  <div class="text-center w-50 mx-auto wrapper_home_nulldata">
-    <div>
+<div class="text-center w-50 mx-auto wrapper_home_nulldata">
+  <div>
     <p class="text-center">Aún no haz registrado a ningún alumno,
           ¡Registra uno para comenzar a organizar su horario!</p>
-  
-  
-      <!-- Button trigger modal -->
-      <a href="/alta-alumno" type="button" class="btn button-home" >
-          Registra a tu hijo aquí
-  </a>
-    </div>
+    <!-- Button trigger modal -->
+    <a href="/alta-alumno" type="button" class="btn button-home" >
+      Registra a tu hijo aquí
+    </a>
   </div>
+</div>
   
-  @stop
+@stop
